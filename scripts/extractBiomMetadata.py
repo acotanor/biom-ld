839d24d602928b63fa3f31f7381fb67ae65c4c2b
acotanor <acotanor@gmail.com>
2026-02-23 12:39:33 +0100

    Añadir modo verbose y extracción raw y rich + corrección de formato: ahora el output se da en líneas distintas.

--- scripts/extractBiomMetadata.py
+++ scripts/extractBiomMetadata.py
@@ -1,12 +1,19 @@
 import biom
 from pathlib import Path
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extractor de metadatos de un archivo .biom")
     parser.add_argument("-i", "--input", help="Ruta del archivo del que extraer los metadatos. Debe ser un archivo .biom.", required=True)
     parser.add_argument("-o", "--output", help="Ruta del archivo de texto que contendrá los metadatos del input.", required=True)
+    parser.add_argument("-v", "--verbose", help="Muestra los metadatos extraidos en la terminal.", action="store_true")
+
+    obs_metadata = parser.add_mutually_exclusive_group(required=True)
+    obs_metadata.add_argument("--raw", help="Modo de extracción; se extraen los metadatos de las observaciones en crudo", action="store_true")
+    obs_metadata.add_argument("--rich", help="Modo de extracción; se extraen los campos de los metadatos de las observaciones de forma separada, se muestra la taxonomía sin la definición del diccionario.", action="store_true")
     
+
     args = parser.parse_args()
 
     try:
@@ -25,17 +32,31 @@ if __name__ == "__main__":
 
     if sobrescribir:
         with open(args.output, 'w') as f:
+            # Metadatos de las muestras.
             for sample_id in table.ids(axis='sample'):
-                # Obtenemos los metadatos de cada muestra
-                t = f"Sample {sample_id} metadata: {table.metadata(id=sample_id, axis='sample')}"
-                print(t)
+                t = f"Sample {sample_id} metadata: {table.metadata(id=sample_id, axis='sample')}\n"
+                if args.verbose:
+                    print(t)
                 f.write(t)
 
-            for observation_id in table.ids(axis='observation'):
-                # Obtenemos los metadatos de cada observación
-                t = f"Observation {observation_id} metadata: {table.metadata(id=observation_id, axis='observation')}"
-                print(t)
-                f.write(t)
+            # Metadatos de las observaciones.
+            if args.raw:
+                for observation_id in table.ids(axis='observation'):
+                    t = f"Observation {observation_id} metadata: {table.metadata(id=observation_id, axis='observation')}\n"
+                    if args.verbose:
+                        print(t)
+                    f.write(t)
+            elif args.rich:
+                for observation_id in table.ids(axis='observation'):
+                    t = f"Observation {observation_id} taxonomy: {';'.join([t for t in table.metadata(id=observation_id, axis='observation').get('taxonomy') if t])}\n"
+                    if args.verbose:
+                        print(t)
+                    f.write(t)
+            else:
+                print("Se debe elegir un modo de extracción de metadatos; --raw o --rich.")
+                exit(1)
+
 
     else:
         print("Operación cancelada.")
+        exit(0)
